37e622b4488cd57cbd1a54c4cbfcae99656c5812
Luis U <80554457+LuisUvE@users.noreply.github.com>
2023-09-14 19:55:14 -0500

    Update Main.java
    Segundo avance

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        // Ejemplo de uso
+        
         LibroDeTextoUNIAJC libroUNIAJC = new LibroDeTextoUNIAJC("Matemáticas Avanzadas", "Profesor X", 45.99, "Matemáticas", "Facultad de Ciencias");
         Novela novela = new Novela("Cien años de soledad", "Gabriel García Márquez", 12.99, "Realista");
 
-        // Imprimir información
+    
         System.out.println("Información del Libro de Texto de la Universidad Antonio José Camacho:");
         libroUNIAJC.imprimirInformacion();
         System.out.println();
