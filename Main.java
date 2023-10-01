22f000da4dc87723c071e0489e9bd8f0f519c1ae
Luis U <80554457+LuisUvE@users.noreply.github.com>
2023-09-30 19:46:31 -0500

    Add files via upload
    first commit

--- Main.java
+++ Main.java
@@ -1,15 +1,49 @@
+package inmuebles;
+
 public class Main {
+
     public static void main(String[] args) {
-        
-        LibroDeTextoUNIAJC libroUNIAJC = new LibroDeTextoUNIAJC("Atlas de anatomia Humana", "Frank H. Netter", 45.65, "Ciencias Humanas", "Facultad de Ciencias");
-        Novela novela = new Novela("La condena sangrienta", "Alejandra Pizarnik", 12.66, "Ficcion");
 
-    
-        System.out.println("Información del Libro de Texto de la Universidad Antonio José Camacho:");
-        libroUNIAJC.imprimirInformacion();
-        System.out.println();
+        // Creamos un inmueble
+        Inmueble inmueble = new Inmueble(1, 100, "Calle 123");
 
-        System.out.println("Información de la Novela:");
-        novela.imprimirInformacion();
+        // Imprimimos los datos del inmueble
+        System.out.println("ID Inmobiliario: " + inmueble.getIdInmobiliario());
+        System.out.println("Metros cuadrados: " + inmueble.getMetrosCuadrados());
+        System.out.println("Dirección: " + inmueble.getDireccion());
+
+        // Creamos una vivienda
+        Vivienda vivienda = new Vivienda(2, 200, "Calle 456", 3, 2);
+
+        // Imprimimos los datos de la vivienda
+        System.out.println("ID Inmobiliario: " + vivienda.getIdInmobiliario());
+        System.out.println("Metros cuadrados: " + vivienda.getMetrosCuadrados());
+        System.out.println("Dirección: " + vivienda.getDireccion());
+        System.out.println("Número de habitaciones: " + vivienda.getNumeroHabitaciones());
+        System.out.println("Número de baños: " + vivienda.getNumeroBaños());
+
+        // Creamos una casa
+        Casa casa = new Casa(3, 300, "Calle 789", 4, 3, 2);
+
+        // Imprimimos los datos de la casa
+        System.out.println("ID Inmobiliario: " + casa.getIdInmobiliario());
+        System.out.println("Metros cuadrados: " + casa.getMetrosCuadrados());
+        System.out.println("Dirección: " + casa.getDireccion());
+        System.out.println("Número de habitaciones: " + casa.getNumeroHabitaciones());
+        System.out.println("Número de baños: " + casa.getNumeroBaños());
+        System.out.println("Cantidad de pisos: " + casa.getCantidadPisos());
+
+        // Creamos una casa rural
+        CasaRural casaRural = new CasaRural(4, 400, "Calle 012", 5, 4, 2, 10, 1000);
+
+        // Imprimimos los datos de la casa rural
+        System.out.println("ID Inmobiliario: " + casaRural.getIdInmobiliario());
+        System.out.println("Metros cuadrados: " + casaRural.getMetrosCuadrados());
+        System.out.println("Dirección: " + casaRural.getDireccion());
+        System.out.println("Número de habitaciones: " + casaRural.getNumeroHabitaciones());
+        System.out.println("Número de baños: " + casaRural.getNumeroBaños());
+        System.out.println("Cantidad de pisos: " + casaRural.getCantidadPisos());
+        System.out.println("Distancia a la cabecera municipal: " + casaRural.getDistanciaCabeceraMunicipal());
+        System.out.println("Altitud sobre el nivel del mar: " + casaRural.getAltitudNivelMar());
     }
 }
